ce08b0b6a4e8ab6445d0f0aabaaf63619386a390
Yuji Harashima <harashimayuji@gmail.com>
2016-03-15 14:25:14 -0600

    Add a new java program

---
+++ src/assignment01/inchesToMillimeters.java
@@ -0,0 +1,15 @@
+package assignment01;
+
+/**
+ * @description inches to millimeters
+ * @author yujiharashima
+ */
+public class inchesToMillimeters {
+
+    public static void main(String[] args) {
+        // Hint: 1 inch = 25.4mm
+        int a = 2, //inches
+                b = 5,
+                c = 10;
+    }
+}
