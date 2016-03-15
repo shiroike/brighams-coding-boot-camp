99d084f49879c5e3bbf417d08625b53099e6fd0b
Yuji Harashima <harashimayuji@gmail.com>
2016-03-15 14:15:18 -0600

    Sequence java file skeleton
    For assignment 01

---
+++ src/assignment01/sequence.java
@@ -0,0 +1,20 @@
+package assignment01;
+
+
+/**
+ * 
+ * The main method in this class shows three circles of different colors and sizes to the screen
+        
+ * @author yujiharashima
+ */
+import java.awt.*;
+import java.applet.Applet;
+ 
+    public class sequence extends Applet
+    {
+       
+        public void paint (Graphics g)
+        
+        {
+        }
+    }
