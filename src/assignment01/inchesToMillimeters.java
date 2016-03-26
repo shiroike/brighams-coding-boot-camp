44a3fd7c3c0ae1b223c60ea2cf305ea09b86471a
Yuji Harashima <harashimayuji@gmail.com>
2016-03-26 15:39:11 -0600

    concert inches to millimeters

--- src/assignment01/inchesToMillimeters.java
+++ src/assignment01/inchesToMillimeters.java
@@ -11,5 +11,15 @@ public class inchesToMillimeters {
         int a = 2, //inches
                 b = 5,
                 c = 10;
+              
+        final double inch = 25.4;
+    
+        double d = a * inch,
+            e = b * inch,
+            f = c * inch;  
+           
+            System.out.println("2 inches is " + d + " millimeters." );
+            System.out.println("5 inches is " + e + " millimeters." );
+            System.out.println("10 inches is " + f + " millimeters." );
     }
 }
