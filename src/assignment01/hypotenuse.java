b38badc9439e2088ed3536e04cbe91948ae94e50
Yuji Harashima <harashimayuji@gmail.com>
2016-03-28 09:46:19 -0600

    Correct code for code A B, create hypotenuse equation

--- src/assignment01/hypotenuse.java
+++ src/assignment01/hypotenuse.java
@@ -12,18 +12,23 @@ public class hypotenuse {
     {
         // use the scanner class to request user input. Be sure to accept double values in case user enters numbers with decimals!
         Scanner keyboard = new Scanner(System.in);
-        
+        double side1, side2, hypotenuse;
+       
+        // scanner class is found textbook pg.129
         System.out.print( "Enter side A: " );
-        String a = keyboard.next();
-        System.out.println( "Side A: "+ a );
+        side1 = keyboard.nextDouble();
+        System.out.println( "Side A: "+ side1 );
         
         System.out.print( "Enter side B:" );
-        String b = keyboard.next();
-        System.out.println( "Side B: " + b );
-        // scanner class is found textbook pg.129
+        side2 = keyboard.nextDouble();
+        System.out.println( "Side B: " + side2 );
         
+        /*Math class is found textbook pg.140
+          Also the equation is found below
+                http://javauk.blogspot.com/2013/07/java-hypotenuse-right-triangle.html
+                */
+        hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
         
-        
-        
+        System.out.println("The hypotenuse of the triangle is " + hypotenuse );
     }
 }
