5274bf90e37d0ebc7c49bb1c72cae5a27f1c52c1
Yuji Harashima <harashimayuji@gmail.com>
2016-03-28 10:40:33 -0600

    converting decimal to binary, reference

---
+++ src/assignment01/binaryNote.java
@@ -0,0 +1,19 @@
+package assignment01;
+/**
+ *
+ * @author yujiharashima
+ */
+public class binaryNote {
+    
+        public static void main(String a[]){
+            
+        ///Refference from online site; http://beginnersbook.com/2014/07/java-program-to-convert-decimal-to-binary/
+            
+    	System.out.println("Binary representation of 124: ");
+    	System.out.println(Integer.toBinaryString(124));
+        System.out.println("\nBinary representation of 45: ");
+        System.out.println(Integer.toBinaryString(45));
+        System.out.println("\nBinary representation of 999: ");
+        System.out.println(Integer.toBinaryString(999));
+}
+}
