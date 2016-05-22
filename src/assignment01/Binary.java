e6c14a0e1c832bee88c48324fefd9d47ac6306c2
Yuji Harashima <harashimayuji@gmail.com>
2016-05-21 22:30:53 -0600

    In the middle of adding the manual coding

--- src/assignment01/Binary.java
+++ src/assignment01/Binary.java
@@ -7,7 +7,10 @@ public class Binary {
  
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        int binaryNumber, binaryNumber2;
+        int binaryNumber, binaryNumber2, binaryNumber3;
+       
+        decimalNumber = keyboard.nextInt();
+        
                 
          System.out.print( "Please enter the dedimal number between 0-255: " );
         binaryNumber = keyboard.nextInt();
@@ -16,5 +19,9 @@ public class Binary {
         System.out.print( "Please enter the dedimal number between 0-255: " );
         binaryNumber2 = keyboard.nextInt();
         System.out.println("The binary number is " + Integer.toBinaryString(binaryNumber2));
+        
+        binaryNumber3 = String.format("%d", decimalNumber % 2);
+        decimalNumber3 = decimalNumber3 / 2;
+        
     }
 }
