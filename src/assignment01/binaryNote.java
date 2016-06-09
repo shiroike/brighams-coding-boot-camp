8782978c1a565e8bfc8833676e203b7667a5d080
Yuji Harashima <harashimayuji@gmail.com>
2016-06-08 19:56:17 -0600

    added the answer on the bottom

--- src/assignment01/binaryNote.java
+++ src/assignment01/binaryNote.java
@@ -15,5 +15,62 @@ public class binaryNote {
         System.out.println(Integer.toBinaryString(45));
         System.out.println("\nBinary representation of 999: ");
         System.out.println(Integer.toBinaryString(999));
-}
+
+        /* answer by Brigahm
+        
+        import java.util.Scanner;
+public class Binary {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int decimalNumber;
+        String binaryNumber;
+
+        System.out.print("Please enter the decimal number between 0-255: ");
+        decimalNumber = keyboard.nextInt();
+
+        /* We divide the number by 2 everytime
+        and get the right-most bit by doing a modulus 2*/
+        
+        /*
+
+        //firstBit
+        binaryNumber = String.format("%d", decimalNumber % 2);
+        decimalNumber = decimalNumber / 2;
+
+        //Second Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Third Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Fourth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Fifth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Sixth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Seventh Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Eigth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        System.out.println("The binary number is " + binaryNumber);
+        */
+                
+        
+        }
+        
+     
 }
