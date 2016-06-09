8b16ab66305c86ff8be822d667c20e74eba1224d
Yuji Harashima <harashimayuji@gmail.com>
2016-06-08 19:32:11 -0600

    punctuation practice done

--- src/assignment02/punctuation2.java
+++ src/assignment02/punctuation2.java
@@ -1,3 +1,4 @@
+
 import javax.swing.JOptionPane;
 public class punctuation2 {
  
@@ -8,10 +9,28 @@ public class punctuation2 {
     showInputDialog takes 2 parameters, parentComponent and message. We don't have a parent component so passing null.
     The message should ask the user to type in a sentence that ends with a punctuation mark.
    */
-  String sentence = JOptionPane.showInputDialog(null, "My request for input");
+  String sentence = JOptionPane.showInputDialog(null, "Enter the punctuation at the end. (. ! or ?) ");
  
   // here you need to write code that gets the last character in the String variable 'sentence'. Save it to 'lastChar'.
-  char lastChar = ;
+  char lastChar = sentence.charAt(sentence.length() -1 );
+  
  
   // here you can use multiple if/else statements or a switch/case statement to determine the type of punctuation the user entered and then inform the user of the result.
+  switch ( lastChar )
+  {    
+      case '.':
+          JOptionPane.showMessageDialog(null, "You entered period");
+          break;
+      case '?':
+          JOptionPane.showMessageDialog(null, "You entered question mark");
+          break;
+      case '!':
+          JOptionPane.showMessageDialog(null, "YOu entered exclamation mark");
+          break;
+      default:
+          JOptionPane.showMessageDialog(null, "I don't know what punctuation");
+          break;
+
+  }
+  }
 }
