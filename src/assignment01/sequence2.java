8778c9924d64a92f6885b613b4761b2a0b6349d3
Yuji Harashima <harashimayuji@gmail.com>
2016-06-08 19:56:40 -0600

    sequence practice adding skeleton

---
+++ src/assignment01/sequence2.java
@@ -0,0 +1,36 @@
+
+package assignment01;
+
+import java.awt.*;
+import java.applet.Applet;
+ 
+/**
+ *
+ * @author yujiharashima
+ */
+public class sequence2 {
+
+
+    public class Sequence extends Applet
+    {
+        /**
+         * The main method in this class shows three circles
+ * of different colors and sizes to the screen
+         *
+         * @author  Brigham Diaz
+         * @version 09-03-08
+         * @ta      11:50
+         */
+        public void paint (Graphics g)
+        {
+            
+            
+            
+            
+            
+            
+            
+        }
+    }    
+    
+}
