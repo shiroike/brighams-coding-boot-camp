6d8668f81c8e108f93e7cb96c0d3ce15c4856cc7
Yuji Harashima <harashimayuji@gmail.com>
2016-06-18 13:20:15 -0600

    for loops

---
+++ src/uDemy/JavaTutorialForCompleteBeginners/forloops.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uDemy.JavaTutorialForCompleteBeginners;
+
+/**
+ *
+ * @author yujiharashima
+ */
+public class forloops {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i < 5; i++){
+         
+            System.out.printf("The value of i is: %d",i);
+            System.out.printf("The value of i is: %d\n",i);
+            System.out.println("Hello");
+            
+            
+        }
+        }
+        
+    }
+
+}
