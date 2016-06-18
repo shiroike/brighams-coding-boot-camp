8dbe46b62ce7ee25c7c9ffaa76b316485b1c538f
Yuji Harashima <harashimayuji@gmail.com>
2016-06-18 13:20:33 -0600

    while loop

---
+++ src/uDemy/JavaTutorialForCompleteBeginners/whileLoops.java
@@ -0,0 +1,27 @@
+package uDemy.JavaTutorialForCompleteBeginners;
+
+/**
+ *
+ * @author yujiharashima
+ */
+public class whileLoops {
+    public static void main(String[] args){
+        
+        int value = 0;
+      /* while (condition)
+        {
+        things to loop
+        }
+              */
+        
+              
+        while(value < 10 )
+        {        
+        System.out.println("Hello" + value);
+        
+        value = value + 1;
+        
+    }
+    }
+        
+}
