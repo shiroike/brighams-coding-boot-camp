c6648647515e8f42313d45903b4591d3a43f5a06
Yuji Harashima <harashimayuji@gmail.com>
2016-06-18 13:46:11 -0600

    if statement

---
+++ src/uDemy/JavaTutorialForCompleteBeginners/ifStatement.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uDemy.JavaTutorialForCompleteBeginners;
+
+/**
+ *
+ * @author yujiharashima
+ */
+public class ifStatement {
+
+    public static void main(String[] args) {
+        int myInt = 35;
+
+        if (myInt < 10) {
+            System.out.println("Yes, it's ture!");
+        }
+        else if(myInt > 20){
+            System.out.println("No, it's false!");
+         }
+        else{
+            System.out.println("None of the above.");
+        }
+
+        }
+
+    }
+
