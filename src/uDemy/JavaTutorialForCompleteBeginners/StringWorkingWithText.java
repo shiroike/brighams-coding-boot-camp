622481311f69ac8d670535f77d7e9bcaa1a0585d
Yuji Harashima <harashimayuji@gmail.com>
2016-06-18 12:33:31 -0600

    string practice

---
+++ src/uDemy/JavaTutorialForCompleteBeginners/StringWorkingWithText.java
@@ -0,0 +1,36 @@
+package uDemy.JavaTutorialForCompleteBeginners;
+
+/**
+ *
+ * @author yujiharashima
+ */
+public class StringWorkingWithText {
+    public static void main(String[] args) {
+        
+        int myInt = 7;
+        // string is a class/ type of object
+        String text = "Hello";
+        
+        String blank = " ";
+        
+        String name = "Bob";
+        
+        String greeting = text + blank + name;
+        
+        System.out.println(greeting);
+        
+        System.out.println("Hello" + " " + "Bob");
+        
+        System.out.println("My integer is: " + myInt);
+        
+        double myDouble = 7.8;
+        
+        System.out.println("My number is "+ myDouble + ".");
+        
+    }
+    
+    
+    
+    
+    
+}
