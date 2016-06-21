4dfc246a46fcb54ddbf0d92c6396723abf14d0d0
Yuji Harashima <harashimayuji@gmail.com>
2016-06-20 21:14:37 -0600

    create the practice class

---
+++ src/uDemy/JavaTutorialForCompleteBeginners/practice.java
@@ -0,0 +1,25 @@
+package uDemy.JavaTutorialForCompleteBeginners;
+
+/**
+ *
+ * @author yujiharashima
+ */
+public class practice {
+    public static void main(String[] args ){
+        
+        for(int i=0; i<5; i++){
+            
+            System.out.println("Hello");
+            
+            
+            
+            
+        }
+            
+       
+        
+        
+    }
+    
+    
+}
