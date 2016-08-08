68a6f2223886aedcff0c071ea9a882595c399fcb
Yuji Harashima <harashimayuji@gmail.com>
2016-08-08 13:10:38 -0600

    convert unit practice

---
+++ src/assignment01/inchesToMillimeters2.java
@@ -0,0 +1,28 @@
+package assignment01;
+
+/**
+ *
+ * @author yujiharashima
+ */
+public class inchesToMillimeters2{
+public static void main(String[] args)
+{
+        // Hint: 1 inch = 25.4mm
+        int a =  2, //inches
+            b =  5,
+            c = 10;
+        
+        
+        double inch = 25.4;
+        double  d = a * inch,
+                e = b * inch,
+                f = c * inch;
+                
+         System.out.println("2 inches is " + d + " millimeters.");
+         System.out.println("5 inches is " + e + " millimeters.");
+         System.out.println("10 inches is " + f + " millimeters.");
+        
+        
+        
+}
+}
