4c74f40ba4f462315b7bcdc51dc5bff536c1c9a9
Yuji Harashima <harashimayuji@gmail.com>
2016-08-09 16:55:04 -0600

    correction and answerKey

--- src/assignment01/Binary.java
+++ src/assignment01/Binary.java
@@ -8,10 +8,7 @@ public class Binary {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         int binaryNumber, binaryNumber2, binaryNumber3;
-       
-        decimalNumber = keyboard.nextInt();
-        
-                
+          
          System.out.print( "Please enter the dedimal number between 0-255: " );
         binaryNumber = keyboard.nextInt();
         System.out.println("The binary number is " + Integer.toBinaryString(binaryNumber));
@@ -20,8 +17,60 @@ public class Binary {
         binaryNumber2 = keyboard.nextInt();
         System.out.println("The binary number is " + Integer.toBinaryString(binaryNumber2));
         
-        binaryNumber3 = String.format("%d", decimalNumber % 2);
-        decimalNumber3 = decimalNumber3 / 2;
+        /*binaryNumber3 = String.format("%d", decimalNumber % 2)
+        decimalNumber3 = decimalNumber3 / 2
+        */
         
+       /*Brighams Answer
+        import java.util.Scanner;
+public class Binary {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int decimalNumber;
+        String binaryNumber;
+
+        System.out.print("Please enter the decimal number between 0-255: ");
+        decimalNumber = keyboard.nextInt();
+
+        /* We divide the number by 2 everytime
+        and get the right-most bit by doing a modulus 2
+
+        firstBit
+        binaryNumber = String.format("%d", decimalNumber % 2)
+        decimalNumber = decimalNumber / 2
+
+        Second Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber)
+        decimalNumber = decimalNumber / 2
+
+        //Third Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber)
+        decimalNumber = decimalNumber / 2
+
+        //Fourth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber)
+        decimalNumber = decimalNumber / 2
+
+        //Fifth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber)
+        decimalNumber = decimalNumber / 2
+
+        //Sixth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber)
+        decimalNumber = decimalNumber / 2
+
+        //Seventh Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber)
+        decimalNumber = decimalNumber / 2
+
+        //Eigth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber)
+        decimalNumber = decimalNumber / 2
+
+        System.out.println("The binary number is " + binaryNumber)
+  
+        */
+
     }
 }
