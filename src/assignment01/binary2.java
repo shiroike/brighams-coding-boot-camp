d9a914c6a3fc68a5836ef59b8745aef487e621d4
Yuji Harashima <harashimayuji@gmail.com>
2016-08-09 17:02:30 -0600

    answerKey

---
+++ src/assignment01/binary2.java
@@ -0,0 +1,55 @@
+package assignment01;
+
+/**
+ *
+ * @author yujiharashima
+ */
+import java.util.Scanner;
+public class binary2 {
+ 
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+         int decimalNumber;
+        String binaryNumber;
+
+        System.out.print("Please enter the decimal number between 0-255: ");
+        decimalNumber = keyboard.nextInt();
+        
+         //firstBit
+        binaryNumber = String.format("%d", decimalNumber % 2);
+        decimalNumber = decimalNumber / 2;
+
+        //Second Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Third Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Fourth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Fifth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Sixth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Seventh Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        //Eigth Bit
+        binaryNumber = String.format("%d%s", decimalNumber % 2, binaryNumber);
+        decimalNumber = decimalNumber / 2;
+
+        System.out.println("The binary number is " + binaryNumber);   
+        
+        
+    }
+}
