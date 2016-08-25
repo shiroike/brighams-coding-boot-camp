d69658b82133d2d5c1f32939e0f6c6a55203b276
Yuji Harashima <harashimayuji@gmail.com>
2016-08-24 21:09:06 -0600

    explain s and d

--- src/assignment01/binary3.java
+++ src/assignment01/binary3.java
@@ -16,7 +16,7 @@ public class binary3 {
      decimalNumber = keyboard.nextInt();
      
      //first bit 13%2=1
-     binaryNumber = String.format("%d", decimalNumber %2 );
+ binaryNumber = String.format("%d", decimalNumber %2 );
      decimalNumber = decimalNumber /2;
      
      //second bit 6%2=0
@@ -28,7 +28,7 @@ public class binary3 {
      decimalNumber = decimalNumber /2;
      
      //fourth bit 1%2=1
-      binaryNumber = String.format("%s%s", decimalNumber %2, binaryNumber);
+      binaryNumber = String.format("%d%s", decimalNumber %2, binaryNumber);
      decimalNumber = decimalNumber /2;
      
      //fifth bit 0%2=0
